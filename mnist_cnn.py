2bbba78c301ceae4259cf39adfe2e4d800908077
Zviksuh90 <zvikush90@gmail.com>
2016-05-27 17:29:08 +0300

    before submit receive param train or test ./mnist_cnn_weights.txt acheiving validation accuracy of 99.2 by epoch 8

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -1,25 +1,25 @@
 from __future__ import print_function
 import numpy as np
-from lxml.html import LabelElement
+import sys
 
 np.random.seed(1337)  # for reproducibility
 
-from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Convolution2D, MaxPooling2D, GaussianNoise
 from keras.optimizers import SGD, Adam
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from keras.utils.np_utils import to_categorical
-from time import gmtime, strftime
 
-WEIGHTS_FILE = "mnist_cnn_weights"+strftime("%Y-%m-%d_%H-%M-%S", gmtime())+".txt"
+WEIGHTS_FILE = "mnist_cnn_weights.txt"
+ID = "204785240.txt"
+
 print(WEIGHTS_FILE)
 batch_size = 50
 nb_classes = 10
-nb_epoch = 100
+nb_epoch = 500
 
 lr = 0.001
 print("Batch Size:" + str(batch_size))
@@ -27,7 +27,8 @@ print("Learning Rate: " + str(lr))
 # input image dimensions
 img_rows, img_cols = 28, 28
 # number of convolutional filters to use
-nb_filters = [32, 64, 128, 128]
+nb_filters = [64, 128, 256, 512]
+print("Filters: " + str(nb_filters))
 # size of pooling area for max pooling
 nb_pool = 2
 # convolution kernel size
@@ -47,25 +48,19 @@ def train_from_file():
     print('Y_train shape:', Y_train.shape)
     print(X_train.shape[0], 'train samples')
 
-    # X_train, Y_train = augmentation.supplement(X_train, Y_train)
-    print("After Augmentation")
-    print('X_train shape:', X_train.shape)
-    print('Y_train shape:', Y_train.shape)
-    print(X_train.shape[0], 'train samples')
-
-    np.save("TrainX", X_train)
-    np.save("TrainY", Y_train)
+    # np.save("TrainX", X_train)
+    # np.save("TrainY", Y_train)
 
     return X_train, Y_train
 
 
-def train_from_past():
-    X_train = np.load("TrainX.npy")
-    Y_train = np.load("TrainY.npy")
-    return X_train, Y_train
+# def train_from_past():
+#     X_train = np.load("TrainX.npy")
+#     Y_train = np.load("TrainY.npy")
+#     return X_train, Y_train
 
 
-X_train, Y_train = train_from_past()
+X_train, Y_train = train_from_file()
 
 Y_train = to_categorical(Y_train, nb_classes)
 
@@ -77,8 +72,8 @@ def val_from_file():
 
     X_val1 = X_val1.reshape(X_val1.shape[0], 1, img_rows, img_cols)
 
-    np.save("ValX1", X_val1)
-    np.save("ValY1", Y_val1)
+    # np.save("ValX1", X_val1)
+    # np.save("ValY1", Y_val1)
 
     csv = np.genfromtxt('./data/validate2.txt', delimiter=",")
     X_val2 = csv[:, 1:785]
@@ -86,23 +81,26 @@ def val_from_file():
 
     X_val2 = X_val2.reshape(X_val2.shape[0], 1, img_rows, img_cols)
 
-    np.save("ValX2", X_val2)
-    np.save("ValY2", Y_val2)
+    # np.save("ValX2", X_val2)
+    # np.save("ValY2", Y_val2)
     return X_val1, Y_val1, X_val2, Y_val2
 
 
-def val_from_past():
-    X_val1 = np.load("ValX1.npy")
-    Y_val1 = np.load("ValY1.npy")
-    X_val2 = np.load("ValX2.npy")
-    Y_val2 = np.load("ValY2.npy")
-    return X_val1, Y_val1, X_val2, Y_val2
+# def val_from_past():
+#     X_val1 = np.load("ValX1.npy")
+#     Y_val1 = np.load("ValY1.npy")
+#     X_val2 = np.load("ValX2.npy")
+#     Y_val2 = np.load("ValY2.npy")
+#     return X_val1, Y_val1, X_val2, Y_val2
 
 
-X_val1, Y_val1, X_val2, Y_val2 = val_from_past()
+X_val1, Y_val1, X_val2, Y_val2 = val_from_file()
 
 Y_val1 = to_categorical(Y_val1, nb_classes)
 Y_val2 = to_categorical(Y_val2, nb_classes)
+
+X_train = np.concatenate((X_train, X_val1), axis=0)
+Y_train = np.concatenate((Y_train, Y_val1), axis=0)
 
 X_train = np.concatenate((X_train, X_val2), axis=0)
 Y_train = np.concatenate((Y_train, Y_val2), axis=0)
@@ -110,31 +108,30 @@ Y_train = np.concatenate((Y_train, Y_val2), axis=0)
 
 def Model(weights_path=None):
     model = Sequential()
-
+    model.add(GaussianNoise(0.01, input_shape=(1, img_rows, img_cols)))
     model.add(Convolution2D(nb_filters[0], nb_conv, nb_conv,
-                            border_mode='valid',
-                            input_shape=(1, img_rows, img_cols)))
+                            border_mode='valid'))
     model.add(Activation('relu'))
 
     model.add(Convolution2D(nb_filters[1], nb_conv, nb_conv))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-    #model.add(Dropout(0.25))
+    model.add(Dropout(0.25))
 
     model.add(Convolution2D(nb_filters[2], nb_conv, nb_conv))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-    #model.add(Dropout(0.25))
+    model.add(Dropout(0.25))
 
     model.add(Convolution2D(nb_filters[3], nb_conv, nb_conv))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-    #model.add(Dropout(0.25))
+    model.add(Dropout(0.25))
 
     model.add(Flatten())
     model.add(Dense(128))
     model.add(Activation('relu'))
-    #model.add(Dropout(0.5))
+    model.add(Dropout(0.5))
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
 
@@ -145,7 +142,7 @@ def Model(weights_path=None):
 
 
 def predict_test(model):
-    csv = np.genfromtxt('./data/validate1.txt', delimiter=",")
+    csv = np.genfromtxt('./data/test.txt', delimiter=",")
     X_val = csv[:, 1:785]
     Y_val = csv[:, 0]
 
@@ -153,7 +150,7 @@ def predict_test(model):
 
     predictions = model.predict_classes(X_val, verbose=True)
     # wrting output to file
-    text_file = open("./mnist-checker/" + WEIGHTS_FILE, "w")
+    text_file = open("./" + ID, "w")
 
     for p in predictions:
         text_file.write(str(p) + "\n")
@@ -163,6 +160,11 @@ def predict_test(model):
 
 def main():
     model = Model()
+    if (sys.argv[1] == "test"):
+        global nb_epoch
+        nb_epoch = 0
+        global WEIGHTS_FILE
+        WEIGHTS_FILE = sys.argv[2]
 
     adam = Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     model.compile(loss='categorical_crossentropy',
@@ -171,19 +173,18 @@ def main():
     datagen = ImageDataGenerator(
         featurewise_center=False,
         featurewise_std_normalization=False,
-        rotation_range=10,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
+        rotation_range=15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
         horizontal_flip=False)
 
     datagen.fit(X_train)
-    callbacks = [ModelCheckpoint(WEIGHTS_FILE, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')]
+    callbacks = [ModelCheckpoint(WEIGHTS_FILE, monitor='val_loss', verbose=1, save_best_only=True, mode='auto'),
+                 EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='auto')]
     model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size),
                         samples_per_epoch=len(X_train), nb_epoch=nb_epoch, validation_data=(X_val1, Y_val1),
                         show_accuracy=True, callbacks=callbacks)
 
-    # model.fit(X_train, Y_train, nb_epoch=nb_epoch, batch_size=batch_size, validation_data=(X_val1, Y_val1), verbose=1,
-    #         show_accuracy=True, callbacks=callbacks)
     model.load_weights(WEIGHTS_FILE)
     predict_test(model)
 
